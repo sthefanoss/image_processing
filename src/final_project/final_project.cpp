cdada7cc8a5a9846741719f5ea1985f1bc88b48b
Sthefano Schiavon <sthefano.schiavon@atlastechnol.com>
2023-01-01 00:17:29 -0300

    use YCbCr to compare colors in final project

--- src/final_project/final_project.cpp
+++ src/final_project/final_project.cpp
@@ -2,8 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#define MAX_HUE 1000
-#define MAX_LUMINANCE 1000
+#define MAX_HUE 255
 #define MAX_SIZE 500
 
 using namespace cv;
@@ -13,19 +12,17 @@ struct showImagesArgs
 {
   String *windowName;
   Mat *input;
+  Mat *inputYCbCr;
   Mat *background;
-  Mat *chromaKey;
-  Mat *result;
-  Vec3b *chromaColor;
-  int *colorThreshold;
-  int *luminanceThreshold;
+  Vec3b *chromaColorYCbCr;
+  int colorDistance;
 };
+
+Vec3b rgb2yCbCr(const Vec3b &pixel);
 
 void showImages(showImagesArgs *args);
 
-void onHueChanged(int pos, void *userdata);
-
-void onLuminanceChanged(int pos, void *userdata);
+void onCbCrDistanceChanged(int pos, void *userdata);
 
 void resizeToMaxSize(Mat &m);
 
@@ -33,29 +30,30 @@ void resizeToMaxSize(Mat &m);
 int main(int argc, char **argv)
 {
   Mat input = imread("input.jpg", IMREAD_COLOR);
-  Mat background = imread("background.jpg", IMREAD_COLOR);
   resizeToMaxSize(input);
+
+  Mat inputYCbCr = Mat::zeros(input.size(), input.type());
+  for (int y = 0; y < input.rows; y++)
+    for (int x = 0; x < input.cols; x++)
+      inputYCbCr.at<Vec3b>(Point(x, y)) = rgb2yCbCr(input.at<Vec3b>(Point(x, y)));
+
+  Mat background = imread("background.jpg", IMREAD_COLOR);
   resize(background, background, input.size());
-  Mat chromaKey;
-  Mat result;
+
   Vec3b chromaColor = Vec3b(0, 255, 0); // Verde
-  int colorThreshold = 700;
-  int luminanceThreshold = 200;
+  Vec3b chromaColorYCbCr = rgb2yCbCr(chromaColor);
+
+  int colorDistance = 118;
   String nomeJanela1 = "Resultado";
 
-  showImagesArgs *args = new showImagesArgs{
-      &nomeJanela1, &input, &background, &chromaKey,
-      &result, &chromaColor, &colorThreshold, &luminanceThreshold};
+  showImagesArgs *args = new showImagesArgs{&nomeJanela1, &input, &inputYCbCr,
+                                            &background, &chromaColorYCbCr, colorDistance};
 
   namedWindow(nomeJanela1, WINDOW_AUTOSIZE);
 
-  createTrackbar("Hue",
-                 nomeJanela1, &colorThreshold,
-                 MAX_HUE, onHueChanged, args);
-
-  createTrackbar("Light",
-                 nomeJanela1, &luminanceThreshold,
-                 MAX_LUMINANCE, onLuminanceChanged, args);
+  createTrackbar("CbCr distance",
+                 nomeJanela1, &colorDistance,
+                 MAX_HUE, onCbCrDistanceChanged, args);
 
   showImages(args);
   waitKey(0);
@@ -64,27 +62,25 @@ int main(int argc, char **argv)
   return 0;
 }
 
-void onHueChanged(int pos, void *userdata)
+Vec3b rgb2yCbCr(const Vec3b &pixel)
 {
-  showImagesArgs *args = (showImagesArgs *)userdata;
-  *args->colorThreshold = pos;
-  showImages(args);
-}
+  return Vec3b{
+      (uchar)round(0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]),
+      (uchar)round(128 + -0.168736 * pixel[0] - 0.331264 * pixel[1] + 0.5 * pixel[2]),
+      (uchar)round(128 + 0.5 * pixel[0] - 0.418688 * pixel[1] - 0.081312 * pixel[2])};
+};
 
-void onLuminanceChanged(int pos, void *userdata)
+void onCbCrDistanceChanged(int pos, void *userdata)
 {
   showImagesArgs *args = (showImagesArgs *)userdata;
-  *args->luminanceThreshold = pos;
+  args->colorDistance = pos;
   showImages(args);
 }
 
 void showImages(showImagesArgs *args)
 {
-  *args->chromaKey = Mat::zeros(args->input->size(), args->input->type());
-  *args->result = Mat::zeros(args->input->size(), args->input->type());
-
-  float tNormalized = (float)*args->colorThreshold / MAX_HUE;
-  float hNormalized = (float)*args->luminanceThreshold / MAX_LUMINANCE;
+  Mat chromaKey = Mat::zeros(args->input->size(), args->input->type());
+  Mat result = Mat::zeros(args->input->size(), args->input->type());
 
   for (int y = 0; y < args->input->rows; y++)
   {
@@ -93,32 +89,26 @@ void showImages(showImagesArgs *args)
       Point p = Point(x, y);
       Vec3b inputPixel = args->input->at<Vec3b>(p);
       Vec3b backgroundPixel = args->background->at<Vec3b>(p);
-      // ajusta se o pixel for 100% preto, para não dar divisão por zero no cálculo de t
-      if (!backgroundPixel[0] && !backgroundPixel[1] && !backgroundPixel[2])
+      Vec3i chroma2YCbCr = (Vec3i)*args->chromaColorYCbCr - (Vec3i)args->inputYCbCr->at<Vec3b>(p);
+      chroma2YCbCr[0] = 0;
+      double distance = sqrt(chroma2YCbCr.ddot(chroma2YCbCr));
+
+      if (distance < args->colorDistance)
       {
-        backgroundPixel[0] = 1;
-        backgroundPixel[1] = 1;
-        backgroundPixel[2] = 1;
-      }
-      // calcula o "ângulo" entre a cor do pixel e a cor do chroma
-      float t = args->chromaColor->ddot(inputPixel) / sqrt(args->chromaColor->ddot(*args->chromaColor) * inputPixel.ddot(inputPixel));
-      float h = sqrt(inputPixel.ddot(inputPixel) / (3 * 255.0 * 255.0));
-      if (t > tNormalized && h > hNormalized)
-      {
-        args->result->at<Vec3b>(p) = backgroundPixel;
-        args->chromaKey->at<Vec3b>(p) = inputPixel;
+        result.at<Vec3b>(p) = backgroundPixel;
+        chromaKey.at<Vec3b>(p) = inputPixel;
       }
       else
       {
-        args->result->at<Vec3b>(p) = inputPixel;
-        args->chromaKey->at<Vec3b>(p) = Vec3b(255, 255, 255);
+        result.at<Vec3b>(p) = inputPixel;
+        chromaKey.at<Vec3b>(p) = Vec3b(255, 255, 255);
       }
     }
   }
 
   Mat h1, h2, v;
   cv::hconcat(*args->input, *args->background, h1);
-  cv::hconcat(*args->chromaKey, *args->result, h2);
+  cv::hconcat(chromaKey, result, h2);
   cv::vconcat(h1, h2, v);
   cv::imshow(*args->windowName, v);
 }
@@ -128,9 +118,9 @@ void resizeToMaxSize(Mat &m)
   if (m.size[0] < MAX_SIZE && m.size[1] < MAX_SIZE)
     return;
 
-  Size size = Size(0,0);
+  Size size = Size(0, 0);
   Size currentSize = m.size();
-  double ratio = (double) currentSize.height / currentSize.width;
+  double ratio = (double)currentSize.height / currentSize.width;
 
   if (ratio > 1)
   {
