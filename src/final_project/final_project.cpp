cc3eda8d78e394ffa248c5f693df6c1ef44e7501
Sthefano Schiavon <sthefano.schiavon@atlastechnol.com>
2023-01-02 01:26:48 -0300

    update final project to video version

--- src/final_project/final_project.cpp
+++ src/final_project/final_project.cpp
@@ -3,193 +3,180 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cmath>
 
-#define MAX_HUE 255
-#define MAX_SIZE 500
-#define COLOR_WEEL_SIZE 512
-using namespace cv;
+#define MAX_SIZE 480
+#define WINDOW_NAME "Chroma key"
+
 using namespace std;
+
+using namespace cv;
 
 typedef struct
 {
-  String *windowName;
-  Mat *foreground;
-  Mat *foregroundYCbCr;
-  Mat *background;
-  Mat *colorWheel;
-  int colorIndex;
-  int colorTolerance;
-} showImagesArgs;
-
-Vec3b rgb2yCbCr(const Vec3b &pixel);
-
-void showImages(showImagesArgs *args);
-
-void showControllers(showImagesArgs *args);
-
-void onCbCrToleranceChanged(int pos, void *userdata);
-
-void onColorChanged(int pos, void *userdata);
+    Mat *foreground;
+    Mat *foregroundAsYCbCr;
+    Mat *background;
+    Mat *colorWheel;
+    int colorIndex;
+    int tolerance;
+} State;
 
 void resizeToMaxSize(Mat &m);
 
-//============================
+Vec3b rgb2YCbCr(const Vec3b &rgb);
+
+Vec3b yCbCr2rgb(const Vec3b &yCbCr);
+
+void onColorChanged(int pos, void *userdata);
+
+void onToleranceChanged(int pos, void *userdata);
+
+void showImages(State *state);
+
 int main(int argc, char **argv)
 {
-  if (argc != 3)
-  {
-    cout << "how to use: ./final_project.out [path to foreground image] [path to background image]" << endl;
-    return -1;
-  }
-  Mat foreground = imread(argv[1], IMREAD_COLOR);
-  if (!foreground.data)
-  {
-    cout << "foreground image not found!" << endl;
-    return -1;
-  }
-  resizeToMaxSize(foreground);
-
-  Mat foregroundYCbCr = Mat::zeros(foreground.size(), foreground.type());
-  for (int y = 0; y < foreground.rows; y++)
-    for (int x = 0; x < foreground.cols; x++)
-      foregroundYCbCr.at<Vec3b>(Point(x, y)) = rgb2yCbCr(foreground.at<Vec3b>(Point(x, y)));
-
-  Mat background = imread(argv[2], IMREAD_COLOR);
-  if (!background.data)
-  {
-    cout << "background image not found!" << endl;
-    return -1;
-  }
-
-  resize(background, background, foreground.size());
-
-  int colorTolerance = 104;
-  String nomeJanela1 = "Resultado";
-  String nomeJanela2 = "Controles";
-
-  namedWindow(nomeJanela1, WINDOW_AUTOSIZE);
-  namedWindow(nomeJanela2, WINDOW_AUTOSIZE);
-  Mat colorWheel = Mat::zeros(Size(COLOR_WEEL_SIZE, 42), foreground.type());
-  for (int x = 0; x < colorWheel.cols; x++)
-  {
-    Vec3b color = Vec3b(140, 127 + 127 * cos(2 * M_PI * x / colorWheel.cols), 127 + 127 * sin(2 * M_PI * x / colorWheel.cols));
-    for (int y = 0; y < colorWheel.rows; y++)
+    // Verifica se os dois parâmetros foram passados
+    if (argc != 3)
     {
-      colorWheel.at<Vec3b>(Point(x, y)) = color;
+        cout << "Por favor, passe o caminho para as imagens de foreground e background assim que execute o programa!" << endl;
+        return -1;
     }
-  }
-  cvtColor(colorWheel, colorWheel, COLOR_YCrCb2BGR);
 
-  int selectedColor = 302;
-
-  showImagesArgs *args = new showImagesArgs{&nomeJanela1, &foreground, &foregroundYCbCr,
-                                            &background, &colorWheel, selectedColor, colorTolerance};
-
-  createTrackbar("Tolerance",
-                 nomeJanela2, &colorTolerance,
-                 MAX_HUE, onCbCrToleranceChanged, args);
-  createTrackbar("Color",
-                 nomeJanela2, &selectedColor,
-                 COLOR_WEEL_SIZE, onColorChanged, args);
-
-  showControllers(args);
-  showImages(args);
-  waitKey(0);
-
-  delete[] args;
-  return 0;
-}
-
-Vec3b rgb2yCbCr(const Vec3b &pixel)
-{
-  return Vec3b{
-      (uchar)round(0.299 * pixel[0] + 0.587 * pixel[1] + 0.114 * pixel[2]),
-      (uchar)round(128 + -0.168736 * pixel[0] - 0.331264 * pixel[1] + 0.5 * pixel[2]),
-      (uchar)round(128 + 0.5 * pixel[0] - 0.418688 * pixel[1] - 0.081312 * pixel[2])};
-};
-
-void onCbCrToleranceChanged(int pos, void *userdata)
-{
-  showImagesArgs *args = (showImagesArgs *)userdata;
-  args->colorTolerance = pos;
-  showImages(args);
-}
-
-void onColorChanged(int pos, void *userdata)
-{
-  showImagesArgs *args = (showImagesArgs *)userdata;
-  args->colorIndex = pos;
-  showControllers(args);
-  showImages(args);
-}
-
-void showImages(showImagesArgs *args)
-{
-  Mat chromaKey = Mat::zeros(args->foreground->size(), args->foreground->type());
-  Mat result = Mat::zeros(args->foreground->size(), args->foreground->type());
-  Vec3b chromaColorAsYCbCr = rgb2yCbCr(args->colorWheel->at<Vec3b>(Point(args->colorIndex, 0)));
-
-  for (int y = 0; y < args->foreground->rows; y++)
-  {
-    for (int x = 0; x < args->foreground->cols; x++)
+    // Verifica se as imagens foram carregadas
+    Mat foreground = imread(argv[1], IMREAD_COLOR);
+    Mat background = imread(argv[2], IMREAD_COLOR);
+    if (!foreground.data)
     {
-      Point p = Point(x, y);
-      Vec3b foregroundPixel = args->foreground->at<Vec3b>(p);
-      Vec3b backgroundPixel = args->background->at<Vec3b>(p);
-
-      Vec3i chroma2YCbCr = (Vec3i)chromaColorAsYCbCr - (Vec3i)args->foregroundYCbCr->at<Vec3b>(p);
-      chroma2YCbCr[0] = 0;
-      double cbCrDistance = sqrt(chroma2YCbCr.ddot(chroma2YCbCr));
-
-      if (cbCrDistance < args->colorTolerance)
-      {
-        result.at<Vec3b>(p) = backgroundPixel;
-        chromaKey.at<Vec3b>(p) = foregroundPixel;
-      }
-      else
-      {
-        result.at<Vec3b>(p) = foregroundPixel;
-        chromaKey.at<Vec3b>(p) = Vec3b(255, 255, 255);
-      }
+        cout << "Caminho para imagem de foreground inválido" << endl;
+        return -1;
     }
-  }
+    if (!background.data)
+    {
+        cout << "Caminho para imagem de background inválido" << endl;
+        return -1;
+    }
 
-  Mat h1, h2, v;
-  cv::hconcat(*args->foreground, *args->background, h1);
-  cv::hconcat(chromaKey, result, h2);
-  cv::vconcat(h1, h2, v);
-  cv::imshow(*args->windowName, v);
+    // redimensiona imagens para tamanho máximo
+    resizeToMaxSize(foreground);
+    resize(background, background, foreground.size());
+
+    // faz roda de cores
+    Mat colorWheel = Mat::zeros(Size(foreground.size().width * 2, 42), foreground.type());
+    for (int x = 0; x < colorWheel.cols; x++)
+    {
+        double angle = (double)2 * M_PI * x / colorWheel.cols;
+        Vec3b color = yCbCr2rgb(Vec3b(140, 126 + 126 * cos(angle), 126 + 126 * sin(angle)));
+        for (int y = 0; y < colorWheel.rows; y++)
+        {
+            colorWheel.at<Vec3b>(Point(x, y)) = color;
+        }
+    }
+    // registra trackbar na janela
+    namedWindow(WINDOW_NAME, WINDOW_AUTOSIZE);
+    int colorIndex = 1;
+    int tolerance = 80;
+
+    Mat foregroundAsYCbCr;
+    cvtColor(foreground,foregroundAsYCbCr,COLOR_RGB2YCrCb);
+    State *state = new State{&foreground, &foregroundAsYCbCr, &background, &colorWheel, colorIndex, tolerance};
+
+
+    createTrackbar("Tolerance", WINDOW_NAME, &tolerance, 255, onToleranceChanged, state);
+    createTrackbar("Color", WINDOW_NAME, &colorIndex, colorWheel.size().width, onColorChanged, state);
+
+    showImages(state);
+
+    waitKey(0);
+    delete[] state;
+    return 0;
 }
 
 void resizeToMaxSize(Mat &m)
 {
-  if (m.size[0] < MAX_SIZE && m.size[1] < MAX_SIZE)
-    return;
+    if (m.size().width <= MAX_SIZE && m.size().height <= MAX_SIZE)
+        return;
 
-  Size size = Size(0, 0);
-  Size currentSize = m.size();
-  double ratio = (double)currentSize.height / currentSize.width;
+    Size size = Size(0, 0);
+    double ratio = (double)m.size().width / m.size().height;
 
-  if (ratio > 1)
-  {
-    size.height = MAX_SIZE;
-    size.width = MAX_SIZE / ratio;
-  }
-  else
-  {
-    size.height = MAX_SIZE * ratio;
-    size.width = MAX_SIZE;
-  }
-
-  resize(m, m, size);
+    if (ratio > 1)
+    {
+        size.width = MAX_SIZE;
+        size.height = MAX_SIZE / ratio;
+    }
+    else
+    {
+        size.width = MAX_SIZE * ratio;
+        size.height = MAX_SIZE;
+    }
+    resize(m, m, size);
 }
 
-void showControllers(showImagesArgs *args)
+Vec3b rgb2YCbCr(const Vec3b &rgb)
 {
+    Mat temp = Mat3b(rgb);
+    cvtColor(temp, temp, COLOR_RGB2YCrCb);
+    return temp.at<Vec3b>(Point(0, 0));
+}
 
-  Mat color = Mat::zeros(args->colorWheel->size(), args->colorWheel->type());
-  for (int y = 0; y < color.rows; y++)
-    for (int x = 0; x < color.cols; x++)
-      color.at<Vec3b>(Point(x, y)) = args->colorWheel->at<Vec3b>(Point(args->colorIndex, 0));
-  cv::vconcat(*args->colorWheel, color, color);
-  cv::imshow("Controles", color);
+Vec3b yCbCr2rgb(const Vec3b &yCbCr)
+{
+    Mat temp = Mat3b(yCbCr);
+    cvtColor(temp, temp, COLOR_YCrCb2RGB);
+    return temp.at<Vec3b>(Point(0, 0));
+}
+
+void onToleranceChanged(int pos, void *userdata) {
+    State *state = (State *)userdata;
+    state->tolerance = pos;
+    showImages(state);
+}
+
+void onColorChanged(int pos, void *userdata)
+{
+    State *state = (State *)userdata;
+    state->colorIndex = pos;
+    showImages(state);
+}
+
+void showImages(State *state)
+{
+    Vec3b chromaKey = state->colorWheel->at<Vec3b>(Point(state->colorIndex, 0));
+    Vec3b chromaKeyAsYCbCr = rgb2YCbCr(chromaKey);
+    Mat selectedColor = Mat::zeros(state->colorWheel->size(), state->colorWheel->type());
+    for (int x = 0; x < selectedColor.cols; x++)
+        for (int y = 0; y < selectedColor.rows; y++)
+            selectedColor.at<Vec3b>(Point(x, y)) = chromaKey;
+
+    Mat mask = Mat::zeros(state->foreground->size(), state->foreground->type());
+    Mat result = Mat::zeros(state->foreground->size(), state->foreground->type());
+
+    for (int x = 0; x < result.cols; x++)
+    {
+        for (int y = 0; y < result.rows; y++)
+        {
+            Vec3b foregroundPixelAsYCbCr = state->foregroundAsYCbCr->at<Vec3b>(Point(x, y));
+            Vec3i difference = (Vec3i)chromaKeyAsYCbCr - (Vec3i)foregroundPixelAsYCbCr;
+            double distance = sqrt(difference[1] * difference[1] + difference[2] * difference[2]);
+
+            if (distance < state->tolerance)
+            {
+                result.at<Vec3b>(Point(x, y)) = state->background->at<Vec3b>(Point(x, y));
+                mask.at<Vec3b>(Point(x, y)) = state->foreground->at<Vec3b>(Point(x, y));
+            }
+            else
+            {
+                result.at<Vec3b>(Point(x, y)) = state->foreground->at<Vec3b>(Point(x, y));
+                mask.at<Vec3b>(Point(x, y)) = Vec3b(255, 255, 255);
+            }
+        }
+    }
+
+    Mat hcat1, hcat2, vcat;
+    hconcat(*state->foreground, *state->background, hcat1);
+    hconcat(mask, result, hcat2);
+    vconcat(*state->colorWheel, selectedColor, vcat);
+    vconcat(vcat, hcat1, vcat);
+    vconcat(vcat, hcat2, vcat);
+
+    imshow(WINDOW_NAME, vcat);
 }
